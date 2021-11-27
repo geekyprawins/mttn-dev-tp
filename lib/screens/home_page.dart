b1185eb4a709d32fc9c585d2d870d3e02e801e7a
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-11-27 13:42:32 +0530

    app icon

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,9 +10,21 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.greenAccent,
     );
   }
 }
+
+//  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+//child: SfPdfViewer.network(
+//         "https://firebasestorage.googleapis.com/v0/b/mttn-dev-tp.appspot.com/o/dse-1.pdf?alt=media&token=d957816c-a876-4a18-9921-04c3a1c40877",
+//         key: _pdfViewerKey,
+//       ),
